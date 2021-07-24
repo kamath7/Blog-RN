25d7308ee2bdf8119dc432560c5067ff868ce338
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-25 02:08:34 +0530

    Setting up reducer

--- src/store/BlogContext.js
+++ src/store/BlogContext.js
@@ -1,16 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer } from "react";
 const BlogContext = createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_BLOG_POST":
+      return [...state, { title: `Blog post ${state.length + 1}` }];
+
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-const [blogPosts, setBlogPosts] = useState([])
-const addBlogPost = ()=>{
-  setBlogPosts([...blogPosts, {title: `Blog Post ${(blogPosts.length + 1)}`} ])
-}
-   
-  return <BlogContext.Provider value={{
-    blogPosts,
-    addBlogPost
-  }}>{children}</BlogContext.Provider>;
+  const [state, dispatch] = useReducer(blogReducer, []);
+
+  return <BlogContext.Provider value={{}}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
