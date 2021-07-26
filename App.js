176642ffe29427e9bbac05632dbb0ea0882ccd3d
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-27 01:39:31 +0530

    Create screen wired

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import React from "react";
 import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
 import { Provider } from "./src/store/BlogContext";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
-    Show: ShowScreen
+    Show: ShowScreen,
+    Create: CreateScreen
   },
   {
     initialRouteName: "Index",
