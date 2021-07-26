890b2d1dd42c69edd412b6dde756dea6fba54088
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-27 01:51:10 +0530

    Styles added. Button added without handling of form yet

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,19 +1,41 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   return (
     <View>
-      <Text>Title </Text>
-      <TextInput value={title} onChangeText={(text) => setTitle(text)} />
-      <Text>Content </Text>
-      <TextInput value={content} onChangeText={(text) => setContent(text)} />
+      <Text style={styles.label}>Title </Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Content </Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title={"Add Blog Post"}/>
     </View>
   );
 };
 
 export default CreateScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "gray",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 15,
+  },
+});
