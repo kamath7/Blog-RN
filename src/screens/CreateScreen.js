997730fdeb43fc3ce8b1f7ddc283e132688cf953
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-27 01:47:51 +0530

    Creating a form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-const CreateScreen = ({navigation}) => {
-    return (
-        <View>
-            <Text>Create Screen </Text>
-        </View>
-    )
-}
+const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  return (
+    <View>
+      <Text>Title </Text>
+      <TextInput value={title} onChangeText={(text) => setTitle(text)} />
+      <Text>Content </Text>
+      <TextInput value={content} onChangeText={(text) => setContent(text)} />
+    </View>
+  );
+};
 
-export default CreateScreen
+export default CreateScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
