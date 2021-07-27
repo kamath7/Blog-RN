cee58894dc388d32bd614be0e965c1766a0ca171
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-28 01:37:09 +0530

    Setting up boilerplate code for sharing a form

---
+++ src/components/BlogForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const BlogForm = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default BlogForm;
+
+const styles = StyleSheet.create({});
