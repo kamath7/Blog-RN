648945f5ed372fbdc834ac55d2a7818b4e2ebff2
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-28 01:42:11 +0530

    Wiring with blogform

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,47 +1,17 @@
-import React, { useContext, useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet,} from "react-native";
 import { Context } from "../store/BlogContext";
-
+import BlogForm from "../components/BlogForm";
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+
   const { addBlogPost } = useContext(Context);
   return (
-    <View>
-      <Text style={styles.label}>Title </Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Content </Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button title={"Add Blog Post"} onPress={()=>{
-
-          addBlogPost(title, content,()=>{
-              navigation.navigate('Index')
-          })}} />
-    </View>
+   <BlogForm/>
   );
 };
 
 export default CreateScreen;
 
 const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "gray",
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 15,
-  },
+
 });
