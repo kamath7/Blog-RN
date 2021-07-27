e3360c658523504790afc5f3b546b4b16ed08276
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-27 17:37:39 +0530

    Setting up Edit icon

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,17 +1,29 @@
-import React, {useContext} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import {Context} from '../store/BlogContext'
-const ShowScreen = ({navigation}) => {
-    const id = navigation.getParam('id')
-    const {state} = useContext(Context)
-    const blogPost = state.find((blog)=> blog.id === id);
-    return (
-        <View>
-            <Text>{blogPost.title}</Text>
-        </View>
-    )
-}
+import React, { useContext } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { EvilIcons } from "@expo/vector-icons";
+import { Context } from "../store/BlogContext";
+const ShowScreen = ({ navigation }) => {
+  const id = navigation.getParam("id");
+  const { state } = useContext(Context);
+  const blogPost = state.find((blog) => blog.id === id);
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+    </View>
+  );
+};
 
-export default ShowScreen
+ShowScreen.navigationOptions = () => {
+  return {
+    headerRight: () => {
+      return (
+        <TouchableOpacity>
+          <EvilIcons name={"pencil"} size={30} />
+        </TouchableOpacity>
+      );
+    },
+  };
+};
+export default ShowScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
